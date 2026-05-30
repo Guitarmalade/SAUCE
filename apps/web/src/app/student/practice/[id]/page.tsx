f987b6365138e89f2515dbb462b23c2818183234
Christopher Schreiner <schreiner.chris@gmail.com>
2026-05-30 11:23:11 -0400

    Fix Supabase server imports for deploy

--- apps/web/src/app/student/practice/[id]/page.tsx
+++ apps/web/src/app/student/practice/[id]/page.tsx
@@ -1,4 +1,4 @@
-import { createClient } from '@/utils/supabase/server'
+import { createClient } from '../../../../utils/supabase/server'
 import { redirect } from 'next/navigation'
 import KitchenClient from './KitchenClient'
 import { SplatBackdrop } from '@/components/ui/Splats'
